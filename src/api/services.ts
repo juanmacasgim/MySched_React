831eddcb3d30def9d59963e1b913442b5f6ecd33
Juan María Castillo Giménez <juanmariacastillogimenez@gmail.com>
2025-05-30 21:21:27 +0200

    Updates

--- src/api/services.ts
+++ src/api/services.ts
@@ -3,7 +3,7 @@ import { UserInterface } from '../interfaces/UserInterface';
 import { CalendarInterface } from '../interfaces/CalendarInterface';
 import { EventInterface } from '../interfaces/EventInterface';
 
-export const API_URL = 'http://localhost:8080//api';
+export const API_URL = 'http://localhost:8080/api';
 
 /**
  * User Singup
